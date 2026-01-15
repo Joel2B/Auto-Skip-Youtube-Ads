d0b000eabcd91ce3c588ca3806aa3c7cd6e4543d
Joel2B <jbuenouas@gmail.com>
2026-01-15 15:19:36 -0700

    updates

--- src/extension/modules/ads.ts
+++ src/extension/modules/ads.ts
@@ -2,7 +2,7 @@ import { getOption } from 'extension/modules/data';
 import { sendMessageBackground } from 'utils/chrome/runtime';
 import { debug } from 'extension/modules/debug';
 import { deepQuerySelector, deepQuerySelectorAll } from 'utils/query';
-import { delay } from 'utils/utils';
+import { waitFor } from 'utils/utils';
 
 let methodExecuted = false;
 
@@ -12,33 +12,51 @@ async function m1() {
   }
 
   try {
-    const adInfoButton = document.querySelector('.ytp-ad-button-icon') as HTMLElement | null;
+    const adInfoButton: HTMLElement | null = await waitFor(() => document.querySelector('.ytp-ad-button-icon'), {
+      timeoutMs: 2000,
+      intervalMs: 100,
+    });
 
     if (!adInfoButton) {
       throw new Error();
     }
 
     adInfoButton.click();
-    await delay(1000);
 
-    const blockButton = deepQuerySelectorAll<HTMLElement | null>('button').find((el) =>
-      el.textContent.includes('Block'),
+    const blockButton: HTMLElement = await waitFor(
+      () => deepQuerySelectorAll<HTMLElement | null>('button').find((el) => el.textContent.includes('Block')),
+      {
+        timeoutMs: 2000,
+        intervalMs: 100,
+      },
     );
 
-    await delay(1000);
-
     if (!blockButton) {
-      const closeButton = deepQuerySelector<HTMLElement>('button[aria-label="Close"]');
-      closeButton.click();
+      const closeButton: HTMLElement = await waitFor(
+        () => deepQuerySelectorAll<HTMLElement>('button[aria-label="Close"]').at(-1),
+        {
+          timeoutMs: 2000,
+          intervalMs: 100,
+        },
+      );
 
+      closeButton.click();
       throw new Error();
     }
 
     blockButton.click();
-    await delay(1000);
 
-    const continueButton = deepQuerySelectorAll<HTMLElement | null>('[role="button"]').find((el) =>
-      el.textContent.includes('CONTINUE'),
+    const continueButton: HTMLElement = await waitFor(
+      () => deepQuerySelectorAll<HTMLElement | null>('[role="button"]').at(-1),
+      {
+        timeoutMs: 4000,
+        intervalMs: 100,
+        minMs: 2000,
+      },
+    );
+
+    console.warn(
+      deepQuerySelectorAll<HTMLElement | null>('[role="button"]').find((el) => el.textContent.includes('CONTINUE')),
     );
 
     if (!continueButton) {
@@ -46,9 +64,16 @@ async function m1() {
     }
 
     continueButton.click();
-    await delay(1000);
 
-    const closeButton = deepQuerySelectorAll<HTMLElement>('button[aria-label="Close"]').at(-1);
+    const closeButton: HTMLElement = await waitFor(
+      () => deepQuerySelectorAll<HTMLElement>('button[aria-label="Close"]').at(-1),
+      {
+        timeoutMs: 2000,
+        intervalMs: 100,
+        minMs: 1000,
+      },
+    );
+
     closeButton.click();
 
     sendMessageBackground({
@@ -109,17 +134,47 @@ function m2() {
   debug('m2', 1);
 }
 
-function m3() {
+async function m3() {
   if (!getOption('m3') || methodExecuted) {
     return;
   }
 
-  const ad = document.querySelectorAll(
-    '.ytp-ad-player-overlay, .ytp-ad-player-overlay-instream-info, .ytp-ad-simple-ad-badge',
-  );
+  try {
+    const ad = deepQuerySelectorAll<HTMLElement>('.ytp-ad-module');
 
-  const video = document.querySelector('video') as HTMLVideoElement | null;
-  if (!video || !isFinite(video.duration) || ad.length == 0) {
+    if (ad.length == 0) {
+      throw new Error();
+    }
+
+    const video: HTMLVideoElement | null = document.querySelector('video');
+
+    if (video && isFinite(video.duration)) {
+      for (let i = 0; i < 5; i++) {
+        video.currentTime += 1;
+      }
+
+      video.currentTime = video.duration;
+    }
+
+    const adButton: HTMLElement = await waitFor(() => deepQuerySelector<HTMLElement>('.ytp-skip-ad-button'), {
+      timeoutMs: 2000,
+      intervalMs: 100,
+    });
+
+    if (adButton) {
+      adButton.click();
+    }
+
+    sendMessageBackground({
+      id: 'analytics',
+      value: {
+        method: 3,
+        status: 1,
+      },
+    });
+
+    debug('m3', 1);
+  } catch (e) {
     sendMessageBackground({
       id: 'analytics',
       value: {
@@ -127,22 +182,10 @@ function m3() {
         status: 0,
       },
     });
-    debug('m3', 0);
-    return;
-  }
 
-  for (let i = 0; i < 5; i++) {
-    video.currentTime += 1;
+    debug('m3', 0);
+    console.error(e);
   }
-  video.currentTime = video.duration;
-  sendMessageBackground({
-    id: 'analytics',
-    value: {
-      method: 3,
-      status: 1,
-    },
-  });
-  debug('m3', 1);
 }
 
 export async function skipAd() {
@@ -150,7 +193,6 @@ export async function skipAd() {
     return;
   }
   debug('skipAd');
-  await delay(1000);
 
   // Method 1 - report the video (inappropriate, repetitive, irrelevant)
   // this method only works in english language
@@ -160,7 +202,7 @@ export async function skipAd() {
   m2();
 
   // Method 3 - advance 5 seconds and force it to end
-  m3();
+  await m3();
 }
 
 export function skipOverlay() {
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,3 +3,31 @@ export const delay = (ms: number) => {
     setTimeout(resolve, ms);
   });
 };
+
+type WaitForOpts = {
+  timeoutMs?: number;
+  intervalMs?: number;
+  minMs?: number;
+};
+
+export async function waitFor<T>(
+  getter: () => T | null | undefined,
+  { timeoutMs = 2000, intervalMs = 100, minMs = 0 }: WaitForOpts = {},
+): Promise<NonNullable<T> | null> {
+  const start = Date.now();
+
+  while (Date.now() - start < timeoutMs) {
+    try {
+      const v = getter();
+      const elapsed = Date.now() - start;
+
+      if (v != null && elapsed >= minMs) return v as NonNullable<T>;
+    } catch (e) {
+      console.error(e);
+    }
+
+    await delay(intervalMs);
+  }
+
+  return null;
+}
