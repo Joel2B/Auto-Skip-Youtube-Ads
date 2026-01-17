acda7c75c2132adb163e6da72f8c680dc8e96cb0
Joel2B <jbuenouas@gmail.com>
2026-01-16 21:24:05 -0700

    updates

---
+++ src/extension/modules/ads/advanceSkip.ts
@@ -0,0 +1,62 @@
+import { getOption } from 'extension/modules/data';
+import { sendMessageBackground } from 'utils/chrome/runtime';
+import { deepQuerySelectorAll } from 'utils/query';
+import { delay, waitFor } from 'utils/utils';
+import { status } from 'extension/modules/ads';
+
+export async function advanceSkip() {
+  if (!getOption('m3') || status.skip) {
+    return;
+  }
+
+  console.log('m3');
+
+  try {
+    await delay(1000);
+
+    const ad = deepQuerySelectorAll<HTMLElement | null>('.ytp-ad-module').at(-1);
+
+    if (!ad || ad.innerHTML == '') {
+      return;
+    }
+
+    const video: HTMLVideoElement | null = document.querySelector('video');
+
+    if (video && isFinite(video.duration)) {
+      for (let i = 0; i < 5; i++) {
+        video.currentTime += 1;
+      }
+
+      video.currentTime = video.duration;
+    }
+
+    await delay(500);
+
+    const adButton = await waitFor(() => deepQuerySelectorAll<HTMLElement | null>('.ytp-skip-ad-button').at(-1), {
+      timeoutMs: 2000,
+      intervalMs: 100,
+    });
+
+    adButton?.click();
+
+    sendMessageBackground({
+      id: 'analytics',
+      value: {
+        method: 3,
+        status: 1,
+      },
+    });
+  } catch (e) {
+    status.skip = true;
+
+    sendMessageBackground({
+      id: 'analytics',
+      value: {
+        method: 3,
+        status: 0,
+      },
+    });
+
+    console.error(e);
+  }
+}
