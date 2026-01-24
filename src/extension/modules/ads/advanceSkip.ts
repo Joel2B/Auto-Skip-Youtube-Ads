3a23616411fdd9c718e85d1f25b1d5036ea038bd
Joel2B <jbuenouas@gmail.com>
2026-01-23 21:12:51 -0700

    updates

--- src/extension/modules/ads/advanceSkip.ts
+++ src/extension/modules/ads/advanceSkip.ts
@@ -1,7 +1,23 @@
 import { getOption } from 'extension/modules/data';
 import { sendMessageBackground } from 'utils/chrome/runtime';
 import { deepQuerySelectorAll } from 'utils/query';
-import { delay, waitFor } from 'utils/utils';
+import { waitFor } from 'utils/utils';
+
+function getBufferRemaining(media: HTMLVideoElement) {
+  const ct = media.currentTime;
+  const b = media.buffered;
+
+  for (let i = 0; i < b.length; i++) {
+    const start = b.start(i);
+    const end = b.end(i);
+
+    if (ct >= start && ct <= end) {
+      return end - ct;
+    }
+  }
+
+  return 0;
+}
 
 export async function advanceSkip() {
   if (!getOption('m3')) {
@@ -23,12 +39,17 @@ export async function advanceSkip() {
 
     console.log(video);
 
-    if (video && isFinite(video.duration)) {
-      for (let i = 0; i < 3; i++) {
-        video.currentTime += 1;
-        await delay(100);
-      }
+    const ready = await waitFor(() => (video.readyState >= 3 && Number.isFinite(video.duration) ? true : null), {
+      timeoutMs: 2000,
+      intervalMs: 100,
+    });
 
+    if (ready) {
+      const bufferRemaining = getBufferRemaining(video);
+      console.log(video.currentTime, bufferRemaining, video.duration);
+
+      video.pause();
+      video.currentTime += bufferRemaining / 2;
       video.currentTime = video.duration;
     }
 
